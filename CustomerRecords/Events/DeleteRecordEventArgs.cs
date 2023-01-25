5700547f1249e0ff3fd7ddbbf47402473ad60e57
Egor Pavshuk <kir197152@gmail.com>
2023-01-25 21:39:03 +0200

    Add event for removing records

---
+++ CustomerRecords/Events/DeleteRecordEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRecords.Events
+{
+    public class DeleteRecordEventArgs
+    {
+        private readonly int _id;
+        public DeleteRecordEventArgs(int id)
+        {
+            _id = id;
+        }
+
+        public int RecordId { get => _id; }
+    }
+}
