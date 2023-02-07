99aaad8e60e8cc5d1598ee392c05666543ab67f3
Egor Pavshuk <kir197152@gmail.com>
2023-02-07 11:59:57 +0200

    Change Customer record model

--- CustomerRecords/Models/CustomerRecord.cs
+++ CustomerRecords/Models/CustomerRecord.cs
@@ -1,9 +1,86 @@
-﻿namespace CustomerRecords.Models
+﻿using CustomerRecords.ViewModels;
+
+namespace CustomerRecords.Models
 {
-    public class CustomerRecord
+    public class CustomerRecord : BindableBase
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        private int id;
+        private string firstName;
+        private string lastName;
+        private bool isEditMode;
+        private bool isReadOnlyMode;
+        private string buttonContent;
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public bool IsEditMode
+        {
+            get => isEditMode;
+            set
+            {
+                if (isEditMode != value)
+                {
+                    isEditMode = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public bool IsReadOnlyMode
+        {
+            get => isReadOnlyMode;
+            set
+            {
+                if (isReadOnlyMode != value)
+                {
+                    isReadOnlyMode = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string ButtonContent
+        {
+            get => buttonContent;
+            set
+            {
+                if (buttonContent != value)
+                {
+                    buttonContent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
     }
 }
