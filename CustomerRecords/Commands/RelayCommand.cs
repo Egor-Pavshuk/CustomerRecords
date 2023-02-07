6e6450b92984620670e41cd0b2fa15bd90e330f0
Egor Pavshuk <kir197152@gmail.com>
2023-02-07 11:59:22 +0200

    Add Relay command class

---
+++ CustomerRecords/Commands/RelayCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CustomerRecords.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => execute(parameter);
+    }
+}
